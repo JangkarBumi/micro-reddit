3bde613ff77ce824ed6a7816309dff48506f4d09
1ba1 <vvvalenti01@yahoo.it>
2019-04-18 11:36:04 +0200

    fix review errors fix linter errors

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -6,6 +6,4 @@ class Comment < ApplicationRecord
   belongs_to :user
   validates :name, presence: true
   validates :body, presence: true
-  validates :user_id, presence: true
-  validates :post_id, presence: true
 end
